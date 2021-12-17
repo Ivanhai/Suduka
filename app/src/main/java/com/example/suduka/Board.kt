dbf5fd6b8d427e72dcbef8f5013de774271aedfb
Luzeq123 <morkovkin465pro@gmail.com>
2021-12-17 11:57:58 +0700

    l

--- app/src/main/java/com/example/suduka/Board.kt
+++ app/src/main/java/com/example/suduka/Board.kt
@@ -9,46 +9,54 @@ import android.util.AttributeSet
 import android.view.View
 import kotlin.math.roundToInt
 
-class Board constructor(context: Context, attributeSet: AttributeSet) : View(context, attributeSet) {
+class Board constructor(context: Context, attributeSet: AttributeSet) :
+    View(context, attributeSet) {
     private val paint = Paint()
+
     init {
         paint.color = Color.BLACK
         paint.strokeWidth = 10f
         paint.textSize = 100f
     }
+
     private var numbers = listOf<List<Int>>()
 
-    val height_size : Int
+    val height_size: Int
         get() = height / numbers.size
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        var liney = 0f
         var linex = 0f
-        for(i in numbers.indices) {
-            for(z in numbers[i]) {
-                val y : Float = i * height_size.toFloat()
-                val x : Float = numbers[i].indexOf(z) * width.toFloat() / 5f
+        for (y in numbers.indices) {
+            for (x in numbers[y].indices) {
+                val y1: Float = y * height_size.toFloat()
+                val x1: Float = x * width.toFloat() / 5f
                 println("$x $y")
-                canvas.drawText(z.toString(), x, y, paint)
+                canvas.drawText(numbers[y][x].toString(), x1, y1, paint)
             }
         }
-        for(i in 0..3) {
-            liney += height / numbers.size
-            canvas.drawLine(0f, liney, width.toFloat(), liney, paint)
+        for (i in 0..numbers.size) {
+            canvas.drawLine(
+                0f,
+                i * height_size.toFloat(),
+                width.toFloat(),
+                i * height_size.toFloat(),
+                paint
+            )
         }
-        for(i in 0..3) {
+        for (i in 0..3) {
             linex += width / numbers[1].size
             canvas.drawLine(linex, 0f, linex, height.toFloat(), paint)
         }
     }
-    fun drawNumber(number : List<List<Int>>) {
+
+    fun drawNumber(number: List<List<Int>>) {
         numbers = number
         invalidate()
     }
 
     @SuppressLint("ClickableViewAccessibility")
-    fun setOnClickPlaceListener(listener : (x : Int, y : Int) -> Unit) {
+    fun setOnClickPlaceListener(listener: (x: Int, y: Int) -> Unit) {
         setOnTouchListener { _, event ->
             listener(
                 (event.x / (width * 5)).roundToInt(),
